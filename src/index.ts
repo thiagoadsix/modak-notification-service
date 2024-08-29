dfa51122620ff7cafc5a5a0d7f4593865f0453b8
Thiago Andrade Silva <thiago.adsix@gmail.com>
2024-08-28 21:35:31 -0300

    feat: implement async notification sending with random delay

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,38 @@ import "module-alias/register";
 import { NotificationGateway } from "@/gateways/notification.gateway";
 import { NotificationServiceImpl } from "@/services/notification-service.impl";
 
-const service = new NotificationServiceImpl(new NotificationGateway());
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
-service.send("news", "dani@email.com", "news 1");
-service.send("news", "dani@email.com", "news 2");
+class AsyncNotificationServiceImpl extends NotificationServiceImpl {
+  override async send(
+    type: string,
+    userId: string,
+    message: string
+  ): Promise<void> {
+    await delay(Math.random() * 1000);
+    return new Promise((resolve) => {
+      super.send(type, userId, message);
+      resolve();
+    });
+  }
+}
 
-service.send("marketing", "madhu@email.com", "marketing 1");
-service.send("marketing", "madhu@email.com", "marketing 2");
-service.send("marketing", "madhu@email.com", "marketing 3");
-service.send("marketing", "madhu@email.com", "marketing 4");
+const service = new AsyncNotificationServiceImpl(new NotificationGateway());
 
-service.send("status", "juan@email.com", "status 1");
-service.send("status", "juan@email.com", "status 2");
-service.send("status", "juan@email.com", "status 3");
+Promise.all([
+  service.send("news", "dani@email.com", "news 1"),
+  service.send("news", "dani@email.com", "news 2"),
+
+  service.send("marketing", "madhu@email.com", "marketing 1"),
+  service.send("marketing", "madhu@email.com", "marketing 2"),
+  service.send("marketing", "madhu@email.com", "marketing 3"),
+  service.send("marketing", "madhu@email.com", "marketing 4"),
+
+  service.send("status", "juan@email.com", "status 1"),
+  service.send("status", "juan@email.com", "status 2"),
+  service.send("status", "juan@email.com", "status 3"),
+]).then(() => {
+  console.log("All notifications processed.");
+});
